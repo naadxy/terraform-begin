d944be687bc2d527ba7bf729c8e215c182ccd763
naadxy <andyshashipal@gmail.com>
2024-07-20 23:43:56 +0800

    comments added

--- us-east-1/vpc.tf
+++ us-east-1/vpc.tf
@@ -1,7 +1,9 @@
+# Define the AWS provider
 provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC
 resource "aws_vpc" "naadxy" {
   cidr_block = "10.0.0.0/24"
 
@@ -10,9 +12,11 @@ resource "aws_vpc" "naadxy" {
   }
 }
 
+# Create private subnets in three different availability zones
 resource "aws_subnet" "naadxy_private" {
   count = 3
   vpc_id = aws_vpc.naadxy.id
+  # Use cidrsubnet() function to generate subnet CIDR block from the VPC CIDR block
   cidr_block = cidrsubnet(aws_vpc.naadxy.cidr_block, 4, count.index)
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 
@@ -21,9 +25,12 @@ resource "aws_subnet" "naadxy_private" {
   }
 }
 
+# Create public subnets in three different availability zones
 resource "aws_subnet" "naadxy_public" {
   count = 3
   vpc_id = aws_vpc.naadxy.id
+  # Use cidrsubnet() function to generate subnet CIDR block from the VPC CIDR block
+  # Add 8 to the count index to ensure the public subnets are in different CIDR blocks
   cidr_block = cidrsubnet(aws_vpc.naadxy.cidr_block, 4, count.index + 8)
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 
@@ -32,6 +39,7 @@ resource "aws_subnet" "naadxy_public" {
   }
 }
 
+# Create an Internet Gateway
 resource "aws_internet_gateway" "naadxy" {
   vpc_id = aws_vpc.naadxy.id
 
@@ -40,6 +48,7 @@ resource "aws_internet_gateway" "naadxy" {
   }
 }
 
+# Create a route table for public subnets and associate it with the Internet Gateway
 resource "aws_route_table" "naadxy_public" {
   vpc_id = aws_vpc.naadxy.id
 
@@ -53,12 +62,14 @@ resource "aws_route_table" "naadxy_public" {
   }
 }
 
+# Associate public subnets with the public route table
 resource "aws_route_table_association" "public" {
   count = 3
   subnet_id = element(aws_subnet.naadxy_public.*.id, count.index)
   route_table_id = aws_route_table.naadxy_public.id
 }
 
+# Create route tables for private subnets
 resource "aws_route_table" "naadxy_private" {
   count  = 3
   vpc_id = aws_vpc.naadxy.id
@@ -68,12 +79,14 @@ resource "aws_route_table" "naadxy_private" {
   }
 }
 
+# Associate private subnets with their respective route tables
 resource "aws_route_table_association" "private" {
   count = 3
   subnet_id = element(aws_subnet.naadxy_private.*.id, count.index)
   route_table_id = element(aws_route_table.naadxy_private.*.id, count.index)
 }
 
+# Create a VPC endpoint for S3 to allow private subnets to access S3 without NAT gateway
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.naadxy.id
   service_name = "com.amazonaws.us-east-1.s3"
